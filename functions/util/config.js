62a61350862dfc8cc67438d03545238d6a7fb19c
Chris <ihure.chris@gmail.com>
2020-09-16 11:56:18 +0300

    fine tuning

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyC-_t-wm4FwseEolC48jbdCZUMw2oLkX-Q",
+  authDomain: "socialape-a592f.firebaseapp.com",
+  databaseURL: "https://socialape-a592f.firebaseio.com",
+  projectId: "socialape-a592f",
+  storageBucket: "socialape-a592f.appspot.com",
+  messagingSenderId: "172098836476",
+  appId: "1:172098836476:web:a1bda76623bab2c2a08479",
+  measurementId: "G-G49EHHV78K"
+};
